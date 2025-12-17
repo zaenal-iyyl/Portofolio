352cd8a73558451044a5cce561822868304d89f2
Zaenalajj <clpmadang@gmail.com>
2025-12-17 09:29:01 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="text-center text-gray-500 text-sm py-6 border-t border-gray-700 mt-20">
-      © {new Date().getFullYear()} Udin. All rights reserved.
+      © {new Date().getFullYear()} zaenDev. All rights reserved.
     </footer>
   )
 }
